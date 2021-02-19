2a0c9dc4de72c94949f6a44327530ad7e7114717
Tsoy Nadezhda <tson00@vse.cz>
2021-02-19 18:14:07 +0100

    upravy

--- www/tson00/cv01/index.php
+++ www/tson00/cv01/index.php
@@ -1,5 +1,4 @@
 <?php
-
 $avatar = 'logo-social.png';
 $surname = 'Tsoy';
 $name = 'Nadya';
@@ -22,53 +21,57 @@ $phone = '911 911 911';
 $email = 'tson00@vse.cz';
 $webpage = 'mypage.com';
 $avaib = 'yes';
-
-
-
-
-
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<style>
-table {
-  border: 1px solid black;
-  width: 50%;
-  margin-left: auto;
-  margin-right: auto;
-
-}
-</style>
+<html lang="cz">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> firemní vizitku  </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<style>
+.styl{
+  margin: auto;
+  width: 70%;
+  margin-top:5%;
+  border-style: groove;
+  border-width: 7px;
+  border-color: lightblue;
+  padding: 10px;
+  top:5%;
+  background: linear-gradient(to bottom, #33ccff 0%, #003300 100%);
+  color: white;
+  text-align: center;
+  font-size: 18px;
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+img{
+  border-radius: 50%;
+}
+</style>
+    <title> Firemní vizitka  </title>
 </head>
 <body>
-    <table>
-    <thead>
-    <tr>
-    <th coslpan = "2"></th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr></tr>
-      <tr><td colspan ="2" ><img src="<?php echo $avatar;?>" alt="logo" width="100" height="100"></td></tr>
-      <tr><td>Příjmení</td><td><?php echo $surname;?></td></tr>
-      <tr><td>Jméno</td><td><?php echo $name ?></td></tr>
-      <tr><td>Věk </td><td><?php echo $age ;?></td></tr>
-      <tr><td>Povolání</td><td><?php echo $position;?></td></tr>
-      <tr><td>Název firmy</td><td><?php echo $companyname;?></td></tr>
-      <tr><td>Ulice</td><td><?php echo $street ;?></td></tr>
-      <tr><td>Číslo popisné</td><td><?php echo $number;?></td></tr>
-      <tr><td>Číslo orientační</td><td><?php echo $numbero;?></td></tr>
-      <tr><td>Město</td><td><?php echo $city;?></td></tr>
-      <tr><td>Telefon</td><td><?php echo $phone;?></td></tr>
-      <tr><td>Email</td><td><?php echo $email;?></td></tr>
-      <tr><td>Webová stránka</td><td><?php echo $webpage;?></td></tr>
-      <tr><td>dostupný</td><td><?php echo $avaib ;?></td></tr>
-        </tbody>
-    </table>
+  <div class="styl">
+   <div class="container-fluid">
+    <div style="clear: left;">
+      <p style="float: left;"><img src="<?php echo $avatar;?>" height="320px" width="390px" border="1px"></p>
+      <p><div class="container-fluid">Příjmení:<?php echo $surname;?><br>
+      Jméno: <?php echo $name; ?><br>
+      Věk: <?php echo $age ;?><br>
+      Povolání: <?php echo $position;?><br>
+      Název firmy: <?php echo $companyname;?><br>
+      Ulice:  <?php echo $street ;?> <br>
+      Číslo popisné: <?php echo $number;?> <br>
+      Číslo orientační: <?php echo $numbero;?> <br>
+      Město:   <?php echo $city;?> <br>
+      Telefon:     <?php echo $phone;?> <br>
+      Email: <?php echo $email;?><br>
+      <a href="https://eso.vse.cz/~tson00/cv01/" style="color:white;">Webová stránka: <?php echo $webpage;?></a><br>
+      dostupný:  <?php echo $avaib ;?></p>
+    </div>
+  </div>
+ </div>
+</div>
 </body>
 </html>
