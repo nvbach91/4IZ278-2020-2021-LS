775b91e6c2f6b8da2ee80fd84ac8de38eed392b3
Alexandra Fedina <feda07@vse.cz>
2021-02-20 21:02:36 +0100

    opraveny ukol

--- www/feda07/cv01/index.php
+++ www/feda07/cv01/index.php
@@ -1,17 +1,17 @@
 <?php
-$jmeno = 'ALEXANDRA';
-$prijmeni = 'FEDINA';
-$povolani = 'Reklamní manažer';
-$firma = 'Styling';
-$cinnost = 'REKLAMA';
-$adresa = 'Vavilova 69/2';
-$mesto = 'Rostov-na-Donu';
+$name = 'ALEXANDRA';
+$surname = 'FEDINA';
+$myJob = 'Reklamní manažer';
+$company = 'Styling';
+$department = 'REKLAMA';
+$address = 'Vavilova 69/2';
+$city = 'Rostov-na-Donu';
 $tel = '+420777666555';
 $mail = 'alex-dzhan@vse.cz';
 $web = 'https://styling.alexdzan.cz';
+
+$age = date_diff(date_create("30.01.2001"), date_create('now'))->y;
 ?>
-
-
 
 
 <!DOCTYPE html>
@@ -20,40 +20,37 @@ $web = 'https://styling.alexdzan.cz';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $firma ?></title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,200&display=swap');
-    </style>
+    <title><?php echo $company ?></title>
     <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
-    <div class="container">
-        <div class="strana-prvni">
-            <h1 class="firma-prvni"><?php echo $firma ?></h1>
-            <div>
-                <img class="logo" src="img.png" alt="logo">
-            </div>
-            <h1 class="firma-prvni"><?php echo $cinnost ?></h1>
+<div class="container">
+    <div class="first-page">
+        <h1 class="company-first"><?php echo $company ?></h1>
+        <div>
+            <img class="logo" src="img.png" alt="logo">
         </div>
-        <div class="strana-druha">
-            <div class="col">
-                <div class="ja">
-                    <h1> <?php echo $jmeno ?> <?php echo $prijmeni ?> - <?php echo date_diff(date_create("30.01.2001"), date_create('now'))->y; ?> let</h1>
-                    <h2> <?php echo $povolani ?> </h2>
-                </div>
-            </div>
-            <div class="col">
-                <div class="info">
-                    <p>Adresa: <?php echo $adresa ?>,</p>
-                    <p> <?php echo $mesto ?></p>
-                    <p> Tel: <?php echo $tel ?></p>
-                    <p> E-mail: <?php echo $mail ?></p>
-                    <p> Web: <?php echo $web ?></p>
-                    <p> dostupná k pracovním nabídkám </p>
-                </div>
+        <h1 class="company-first"><?php echo $department ?></h1>
+    </div>
+    <div class="second-page">
+        <div class="col">
+            <div class="me">
+                <h1> <?php echo $name ?> <?php echo $surname ?> - <?php echo $age ?> let</h1>
+                <h2> <?php echo $myJob ?> </h2>
             </div>
         </div>
+        <div class="col">
+            <div class="info">
+                <p>Adresa: <?php echo $address ?>,</p>
+                <p> <?php echo $city ?></p>
+                <p> Tel: <?php echo $tel ?></p>
+                <p> E-mail: <?php echo $mail ?></p>
+                <p> Web: <?php echo $web ?></p>
+                <p> dostupná k pracovním nabídkám </p>
+            </div>
+        </div>
+    </div>
 </div>
 
 </body>
