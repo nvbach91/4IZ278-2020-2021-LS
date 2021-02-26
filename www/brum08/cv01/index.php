5db7c49c5743cca85a545093728310de808c2f8c
Michael Brůna <miskobruna@gmail.com>
2021-02-26 19:29:50 +0100

    brum08-cv01

--- www/brum08/cv01/index.php
+++ www/brum08/cv01/index.php
@@ -13,19 +13,21 @@ $avaibility = 'avaible';
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Vizitka</title>
-</head>
-<body>
-<div style="text-align: center; border-style: solid;">
-<h1><?php echo $company ?></h1>
-<p><?php echo $name . ' ' . $lastName ?></p>
-<p><?php echo $age ?></p>
-<p><?php echo $street . ' ' . $streetNum . ' ' . $town ?></p>
-<p><?php echo $phone ?></p>
-<p><?php echo $email ?></p>
-<h3><?php echo $avaibility ?></h3>
-</div>
 
+<head>
+    <title>Vizitka</title>
+    <link rel="stylesheet" href="./css.css">
+</head>
+
+<body>
+    <div class="border">
+        <h1><?php echo $company ?></h1>
+        <p><?php echo $name . ' ' . $lastName ?></p>
+        <p><?php echo $age ?></p>
+        <p><?php echo $street . ' ' . $streetNum . ' ' . $town ?></p>
+        <p><?php echo $phone ?></p>
+        <p><?php echo $email ?></p>
+        <h3><?php echo $avaibility ?></h3>
 </body>
+
 </html>
