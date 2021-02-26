e15494b085c1de33e4f15da872f659df3115b9d3
ditm01 <79308970+ditm01@users.noreply.github.com>
2021-02-26 18:55:18 +0100

    ditm01-cv02
    odevzdávám úkol z cvičeni 2

---
+++ www/ditm01/cv02/index.php
@@ -0,0 +1,7 @@
+
+<?php include './includes/header.php' ?>
+    <div class ="all">
+        <h1>We are superheroes</h1>
+        <?php include './includes/main.php' ?>
+    </div>
+<?php include './includes/footer.php' ?>
