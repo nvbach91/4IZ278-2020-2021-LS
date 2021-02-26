8d195f80b5049f39e8af2813de7c2813205d2c0b
Timo Paluš <58347555+tpalus@users.noreply.github.com>
2021-02-26 12:30:20 +0100

    palt04-cv02

---
+++ www/palt04/cv02/index.php
@@ -0,0 +1,6 @@
+<?php require './header.php'; ?>
+<main class="container">
+    <h1 class="text-center">Business Cards in PHP</h1>
+    <?php require './business-cards.php'; ?>
+</main>
+<?php require './footer.php'; ?>
