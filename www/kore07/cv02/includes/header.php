edcc76f7717116e640c1511b4c71b9cef0540607
Kortoshkina Ekaterina <liberty_12@mail.ru>
2021-02-26 13:25:22 +0100

    kore07-cv02

---
+++ www/kore07/cv02/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Newsreader:wght@400;600&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
