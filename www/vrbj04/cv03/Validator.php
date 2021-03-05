cf4dcd4e12395243b6463e2ee509a25422a27892
Jiří Vrba <jiri@vrba.dev>
2021-03-05 12:10:37 +0100

    Better email validation

--- www/vrbj04/cv03/Validator.php
+++ www/vrbj04/cv03/Validator.php
@@ -17,7 +17,7 @@ final class Validator
         $rules = [
             'name' => strlen($this->data['name']) > 3,
             'gender' => in_array($this->data['gender'], ["m", "f"]),
-            'email' => strlen($this->data['email']) > 4 && strpos($this->data['email'], "@") !== false,
+            'email' => strlen($this->data['email']) > 4 && filter_var($this->data['email'], FILTER_VALIDATE_EMAIL),
             'phone' => strlen($this->data['phone']) > 8 && preg_match("/^[0-9+\s]+$/", $this->data['phone']),
             'avatarURL' => preg_match("/^https?:\/\//", $this->data['avatarURL']),
             'deckName' => strlen($this->data["deckName"]) > 0,
