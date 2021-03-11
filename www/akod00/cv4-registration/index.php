b5d5ede2a633b3a8ef2a0da0539146138f87cf65
Denis Akopyan <denis.akopyan@seznam.cz>
2021-03-11 22:51:59 +0100

    Prepare cv4

---
+++ www/akod00/cv4-registration/index.php
@@ -0,0 +1,6 @@
+<?php
+  $pageTitle = "Home";
+?>
+<?php require __DIR__ . './components/header.inc.php'; ?>
+  <h1>This is my home page</h1>
+<?php require __DIR__ . './components/footer.inc.php'; ?>
---
+++ www/akod00/cv4-registration/pages/about.php
@@ -0,0 +1,6 @@
+<?php
+  $pageTitle = "About";
+?>
+<?php require __DIR__ . './../components/header.inc.php'; ?>
+<h1>This is my about page</h1>
+<?php require __DIR__ . './../components/footer.inc.php'; ?>
