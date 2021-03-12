beb2410938c811a91753cfa6f99bfb84a57f7393
Timotej Paluš <58347555+tpalus@users.noreply.github.com>
2021-03-12 23:04:01 +0100

    palt04-cv04
    odovzdavam cvicenie 4

---
+++ www/palt04/cv04/includes/navigation.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CV04</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="./index.php">Home</a>
+        <a class="nav-link" href="./users.php">Users</a>
+      </div>
+    </div>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="./login.php">Login</a>
+        <a class="nav-link" href="./registration.php">Registration</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="d-flex justify-content-center">
---
+++ www/palt04/cv04/index.php
@@ -0,0 +1,6 @@
+<?php include __DIR__ . '/includes/header.php'; ?>
+<?php include __DIR__ . '/includes/navigation.php'; ?>
+<main>
+    <h1>Welcome to my page</h1>
+</main>
+<?php include __DIR__ . '/includes/footer.php'; ?>
