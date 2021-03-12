a503ab5269b63bae8ad91d69bab1cb78d27b637e
vonm10 <72431403+vonm10@users.noreply.github.com>
2021-03-12 16:34:14 +0100

    cv04

---
+++ www/vonm10/cv04/index.php
@@ -0,0 +1,12 @@
+
+
+<?php include './includes/head.php'; ?>
+<nav>
+    <a href="./index.php">Homepage</a>
+    <a href="./registration.php">Registration</a>
+    <a href="./login.php">Login</a>
+</nav>
+<main>
+<h1>Welcome</h1>
+</main>
+<?php include './includes/foot.php'; ?>
