cfa794f683aa1bc4b38e5d4ad3ae977b6ac48c61
bejjak <79199546+bejjak@users.noreply.github.com>
2021-03-12 16:50:26 +0100

    bejj01-cv04
    Odevzdavam ukol z cviceni 04
    (Uvodni Stranka na esu)[https://eso.vse.cz/~bejj01/cv04]

---
+++ www/bejj01/cv04/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Page</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+
+<body class="bg-secondary text-white">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+            <a class="navbar-brand" href="./index.php">My Page</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="./registration-form.php">Registration</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./login-form.php">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    
