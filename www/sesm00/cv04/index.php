48dc65aa52e7315765c5505706d1b5473b88ca2e
sesm00 <79167950+sesm00@users.noreply.github.com>
2021-03-14 12:35:13 +0100

    cv04

---
+++ www/sesm00/cv04/index.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/includes/core.php';
+include __DIR__ . '/includes/head.php';
+include __DIR__ . '/includes/navigation.php';
+?>
+
+<div class="login-page">
+    <div class="ml-auto mr-auto">
+        <h2>Use navigation above</h2>
+    </div>
+</div>
+
+
+<?php
+include __DIR__ . '/includes/foot.php';
+?>
