2c1794f8edec651fb3d45ec0c3e9b5ef8da7f37c
= <mart10@vse.cz>
2021-03-14 18:21:39 +0100

    cv04

---
+++ www/mart10/cv04/index.php
@@ -0,0 +1,20 @@
+<?php include './includes/header.php' ?>
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+    <div class="inner">
+        <h3 class="masthead-brand">Form</h3>
+        <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href=".">Home</a>
+        <a class="nav-link" href="registration.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+        <a class="nav-link" href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+        </nav>
+    </div>
+    </header>
+    
+    <main role="main" class="inner cover">
+    <h1 class="text-center">Homepage</h1>
+    <h4>Welcome to this website!</h4>
+    <p>On this website you can <a href="registration.php">register</a> and <a href="login.php">login</a> how many times you want </p>
+    </main>
+</div>
+<?php include './includes/footer.php' ?>
