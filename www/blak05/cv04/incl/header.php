98939dd5d576163dc59365b40a39e6857e17efe7
krystofblak05 <79277363+krystofblak05@users.noreply.github.com>
2021-03-17 11:55:01 +0100

    Blak05-cv04
    https://eso.vse.cz/~blak05/cv04/

---
+++ www/blak05/cv04/incl/header.php
@@ -0,0 +1,40 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{text-align: center;}
+        .person{
+            border: 1px solid black;
+            max-width: 300px;
+            text-align: center;
+            margin: 10px auto;
+        }
+        nav, .form{
+            display: flex;
+            max-width: 300px;
+            margin: 0 auto;
+            border: 1px solid black;
+            padding: 20px;
+            justify-content: space-around;
+        }
+        nav a{
+            display: block;
+            text-decoration: none;
+            color: black;
+        }
+        input{
+            display: block;
+            margin: 5px 0;
+        }
+        .warning{
+            display:none;
+            border: 1px solid black;
+            max-width: 300px;
+            text-align: center;
+            margin: 10px auto; 
+        }
+    </style>
+</head>
+<body>
