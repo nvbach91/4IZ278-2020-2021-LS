8cf7810ac80c9327514b9a2c00ee51f5cbe95917
vonm10 <72431403+vonm10@users.noreply.github.com>
2021-04-11 21:07:00 +0200

    Ukol na cviceni 8
    Eso je updated cv06 tzn https://eso.vse.cz/~vonm10/cv06/
    Admin cast dostupna na https://eso.vse.cz/~vonm10/cv06/admin.php

---
+++ www/vonm10/cv08/db/DatabaseOperations.php
@@ -0,0 +1,9 @@
+<?php 
+
+interface DatabaseOperations {
+    public function fetchAll();
+    public function fetch($id);
+    public function add($name, $price, $img);
+    public function update($id, $name, $price, $img);
+    public function delete ($id);
+}
