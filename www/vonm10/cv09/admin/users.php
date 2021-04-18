fb49501994e11360f88b2130de85de8f4accaead
vonm10 <72431403+vonm10@users.noreply.github.com>
2021-04-18 10:43:43 +0200

    cv09
    ESO zase na cv06: https://eso.vse.cz/~vonm10/cv06/
    Pro funkce admina credentials admin@admin.com 1234567890

---
+++ www/vonm10/cv09/admin/users.php
@@ -0,0 +1,53 @@
+<?php require_once __DIR__ . '/../db/UsersDB.php'; ?>
+
+<?php
+session_start();
+if ($_SESSION['admin'] != 3) {
+    header('Location: /./~vonm10/cv06/index.php');
+}
+
+$usersDB = new UsersDB();
+$userRecords = $usersDB->fetchAll();
+
+if (!empty($_POST)) {
+    $usersDB->updateRole($_POST['button'], $_POST['newRole']);
+}
+
+?>
+
+
+
+
+<?php require __DIR__ . '/../incl/header.php'; ?>
+<h1>Users</h1>
+<form method="POST">
+    <table border="1">
+        <tr>
+            <th>id</th>
+            <th>email</th>
+            <th>password</th>
+            <th>role</th>
+            <th>new role</th>
+            <th>confirm</th>
+        </tr>
+        <?php foreach ($userRecords as $userRecord) : ?>
+
+            <tr>
+                <td><?php echo $userRecord['id'] ?></td>
+                <td><?php echo $userRecord['email'] ?></td>
+                <td><?php echo $userRecord['password'] ?></td>
+                <td><?php echo $userRecord['admin'] ?></td>
+                <td>
+                    <select name="newRole">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                </td>
+                <td><button name="button" value=<?php echo $userRecord['id'] ?>>Confirm</button></td>
+            </tr>
+
+        <?php endforeach ?>
+    </table>
+</form>
+<?php require __DIR__ . '/../incl/footer.php'; ?>
