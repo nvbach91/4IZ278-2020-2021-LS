55b39ef555d435dccf025558e342ab1d7fa07fdd
zjaa00 <77669833+socka25@users.noreply.github.com>
2021-04-24 19:53:23 +0200

    zjaa00-cv06
    Dodatočne odovzdávam cvičenie 06

---
+++ www/zjaa00/cv06/partials/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>cv06</title>
+
+  <!-- icon and fonts -->
+  <link rel="icon" href="img/donut.png" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" />
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+    <!script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+    </!script> <![endif]-->
+
+</head>
+
+<body>
