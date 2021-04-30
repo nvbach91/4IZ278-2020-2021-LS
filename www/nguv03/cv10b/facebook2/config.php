64fd1708d0831bc18c6ab905a5c7ed3f779fc27f
Nguyen Viet Bach <nvbach91@outlook.com>
2021-04-30 14:23:29 +0200

    added facebook2

---
+++ www/nguv03/cv10b/facebook2/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+const APP_ID = '800292233940491';
+const APP_SECRET = '3932b49cd15439f3f10cf0b6fac27977';
+
+const LOGIN_CALLBACK_URL = 'http://localhost/4IZ278-2020-2021-LS/www/nguv03/cv10b/facebook2/facebook-login-callback.php';
+
+
+?>
