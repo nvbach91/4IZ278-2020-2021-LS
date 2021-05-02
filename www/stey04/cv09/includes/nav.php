baee49c8d43ebf8112a65636d75b7a7cb7ea6111
stey04 <stey04@vse.cz>
2021-05-02 15:47:42 +0200

    Submit cv09

---
+++ www/stey04/cv09/includes/nav.php
@@ -0,0 +1,32 @@
+<?php session_start(); ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="https://eso.vse.cz/~nguv03/cv08/img/logo.png" width="30" height="30" alt="">
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item <?php echo strpos($_SERVER['REQUEST_URI'], 'index') ? ' active' : '' ?>">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <?php if (@$_SESSION['user_id']) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+                <li class="nav-item <?php echo strpos($_SERVER['REQUEST_URI'], 'cart') ? ' active' : '' ?>">
+                    <a class="nav-link" href="cart.php">Cart</a>
+                </li>
+            <?php else : ?>
+                <li class="nav-item <?php echo strpos($_SERVER['REQUEST_URI'], 'login') ? ' active' : '' ?>">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item <?php echo strpos($_SERVER['REQUEST_URI'], 'signup') ? ' active' : '' ?>">
+                    <a class="nav-link" href="signup.php">Sign Up</a>
+                </li>
+                <li class="nav-item <?php echo strpos($_SERVER['REQUEST_URI'], 'users') ? ' active' : '' ?>">
+                    <a class="nav-link" href="users.php">Users</a>
+                </li>
+            <?php endif ?>
+        </ul>
+    </div>
+</nav>
