1170f9932f312aa6e3c454fa67fb588050b22334
petr-klepetko <klepetkope@gmail.com>
2021-05-08 00:11:05 +0200

    Blady a oprava routes

--- www/neo-nasa/routes/web.php
+++ www/neo-nasa/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\GalaxyController;
+use App\Http\Controllers\SpaceStationController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
