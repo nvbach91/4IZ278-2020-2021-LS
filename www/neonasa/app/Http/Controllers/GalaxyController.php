2bf0bcc988d25a50d5762199d5de0abb155b5056
Denis Akopyan <denis.akopyan@seznam.cz>
2021-05-07 12:16:08 +0200

    Add controllers

---
+++ www/neonasa/app/Http/Controllers/GalaxyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GalaxyController extends Controller
+{
+    public function fetchAll() {
+
+    }
+}
---
+++ www/neonasa/app/Http/Controllers/SpaceStationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SpaceStationController extends Controller
+{
+    public function fetchAll() {
+
+    }
+}
