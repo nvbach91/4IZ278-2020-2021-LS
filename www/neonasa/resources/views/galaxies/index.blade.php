ae71acd48b34570859aa64a08f9f89d8f7520783
Jiří Vrba <jiri@vrba.dev>
2021-05-13 14:56:20 +0200

    Use common parent template instead of copypasting

--- www/neonasa/resources/views/galaxies/index.blade.php
+++ www/neonasa/resources/views/galaxies/index.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GX in space</title>
-</head>
-<body>
+@extends("layout.master")
+@section("content")
 <div class="card" style="width: 18rem;">
     <div class="card-header">
       Galaxies
@@ -17,5 +10,4 @@
       @endforeach
     </ul>
   </div>
-</body>
-</html>
+@endsection
--- www/neonasa/resources/views/galaxies/show.blade.php
+++ www/neonasa/resources/views/galaxies/show.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends("layout.master")
+@section("content")
 <div class="card" style="width: 18rem;">
 
 <img src="<?= $data["info"]["img"] ?>" class="card-img-top">
@@ -30,5 +23,4 @@
 </ul>
 
 </div>
-</body>
-</html>
+@endsection
