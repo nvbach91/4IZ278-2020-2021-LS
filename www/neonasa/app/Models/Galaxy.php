e3eec5957a08010ae1738b78a1d514441797a8a5
Jiří Vrba <jiri@vrba.dev>
2021-05-13 15:35:17 +0200

    Add explicit return types of relations

--- www/neonasa/app/Models/Galaxy.php
+++ www/neonasa/app/Models/Galaxy.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Galaxy extends Model
 {
@@ -15,7 +16,7 @@ class Galaxy extends Model
         'img'
     ];
 
-    public function spaceStations() {
-        $this->hasMany(SpaceStation::class);
+    public function spaceStations(): HasMany {
+        return $this->hasMany(SpaceStation::class);
     }
 }
--- www/neonasa/app/Models/SpaceStation.php
+++ www/neonasa/app/Models/SpaceStation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SpaceStation extends Model
 {
@@ -15,7 +16,7 @@ class SpaceStation extends Model
         'img',
     ];
 
-    public function galaxy() {
+    public function galaxy(): BelongsTo {
         return $this->belongsTo(Galaxy::class);
     }
 }
