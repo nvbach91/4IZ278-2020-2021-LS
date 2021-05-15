19f2337082727e4c1e5ff07db49383afd0be89fd
petr-klepetko <klepetkope@gmail.com>
2021-05-15 21:17:22 +0200

    first few tables

---
+++ www/klep03/sp/database/migrations/2021_05_15_161831_create_songs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique;
+            $table->longText('lyrics_w_chords');
+            $table->string('artist');
+            $table->enum('difficulty', array('beginner', 'easy', 'medium', 'hard'));
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('Users');
+            $table->integer('style')->unsigned();
+            $table->foreign('style')->references('id')->on('Styles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
