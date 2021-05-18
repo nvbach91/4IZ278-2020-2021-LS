282b3a161374377489ee42a7fd4726b9d3d1dd78
Mariola Kadlubiec <mariola.kadlubiec@gmail.com>
2021-05-18 22:04:24 +0200

    sp-kadm08

---
+++ www/kadm08/sp/contact.php
@@ -0,0 +1,71 @@
+<?php
+$errorMessages = [];
+
+function sendMail($name, $email, $message)
+{
+    $recepient = 'kadm08@vse.cz';
+    $subject = $name;
+    $headers = [
+        'MIME-Version: 1.0',
+        'Content-type: text/html, charset=utf-8',
+        'From: ' . $email,
+        'Reply-To: ' . $email,
+        'X-Mailer: PHP/8.0',
+    ];
+    return mail($recepient, $subject, $message, implode("\r\n", $headers));
+}
+
+if (!empty($_POST)) {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    if (!$name || !preg_match('/^[a-zA-Z ]*$/', $name)) {
+        array_push($errorMessages, 'Please enter your name');
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errorMessages, 'Please enter valid email address');
+    }
+
+    if (empty($errorMessages)) {
+        sendMail($name, $email, $message);
+    }
+}
+?>
+
+
+<?php include __DIR__ . '/includes/header.php'; ?>
+
+<br></br><br></br>
+<div class="container contact-form">
+            <form method="post">
+                <h3>Drop Us a Message</h3>
+                <?php foreach ($errorMessages as $message) : ?>
+                    <p style="color:red;"><?php echo $message; ?></p>
+                    <?php endforeach; ?>
+               <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" id="email" name="email" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" id="text" name="phone" class="form-control" placeholder="Your Phone Number *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btnSubmit" class="btn btn-light px-5  shadow-sm" value="Send Message" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
+</div>
+
+<?php include __DIR__ . '/includes/footer.php'; ?>
---
+++ www/kadm08/sp/includes/footer.php
@@ -0,0 +1,70 @@
+<footer class="text-center text-lg-start bg-light text-muted">
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+    </section>
+
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i> Coworking Smetana
+                    </h6>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Contact
+                    </h6>
+                    <p><i class="fas fa-home me-3"></i> Smetanova 217/22, 737 01 Český Těšín</p>
+                    <p>
+                        <i class="fas fa-envelope me-3"></i>
+                        coworkingsmetana@gmail.com
+                    </p>
+                    <p><i class="fas fa-phone me-3"></i> + 420 234 567 889</p>
+                    <p><i class="fas fa-print me-3"></i> + 420 234 567 899</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2021 Copyright:
+        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
+<?php require '../hotreloader.php'; ?>
+</body>
+
+</html>
